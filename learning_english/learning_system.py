8be7f2fd563e5b33167349b3601648ec60101d86
zhy <zhy@123.com>
2026-01-19 19:36:18 +0800

    英语学习系统0.0.6 增加随机挑战功能 随机100题 修改描述 增加拼写过程中可退出选项

--- learning_english/learning_system.py
+++ learning_english/learning_system.py
@@ -3,7 +3,7 @@
 存入数据库使用sqlmodel -> 定义数据库连接的包
 """
 from sqlmodel import select
-
+from sqlalchemy.sql import func
 from sql_connect.sqllite_connect import SessionDep, English, create_session
 
 
@@ -20,6 +20,7 @@ class LearningSystem(object):
 
         print('5,根据中文意思拼写英文')
         print('6,根据英文翻译中文意思')
+        print('7,随机100题得分挑战!')
 
     # 定义存入数据库方法
     def save_to_db(self, session: SessionDep, e_word=None, e_translation=None):
@@ -50,13 +51,19 @@ class LearningSystem(object):
             n = int(n)
             result = session.exec(select(English).limit(n)).all()
         for i in result:
-            if input(f'根据意思拼写单词, {i.e_translation} ') == i.e_word:
+            user_input = input(f'根据意思拼写单词, "{i.e_translation} "的单词是? 输入"exit"可强制退出! ')
+            # 加一层判断可以在运行过程中还能退出
+            if user_input == 'exit':
+                return
+            if user_input == i.e_word:
                 print('正确')
                 count += 1
             else:
                 print(f'错误, 正确的拼写应该为{i.e_word}')
                 error_word[i.e_translation] = i.e_word
-        print(f'总共答对的次数为{count}, 正确率{count / len(result):.2%}')
+        else:
+            score = count / len(result) * 100
+            print(f'总共答对的次数为{count}, 正确率{count / len(result):.2%}, 得分为{score:.1f}')
         if count / len(result) < 1:
             while True:
                 if len(error_word) > 0:
@@ -133,6 +140,51 @@ class LearningSystem(object):
                 self.save_to_db(session, e_word=word, e_translation=translation)
 
 
+    def get_random_100(self,session: SessionDep):
+        """
+        随机获取100到练习
+        :return:
+        """
+        count = 0
+        # 定义空字典,用于临时存放错误的
+        error_word = {}
+        result = session.exec(select(English).order_by(func.random()).limit(100)).all()
+        print(result)
+        for i in result:
+            user_input = input(f'根据意思拼写单词, "{i.e_translation} "的单词是? 输入"exit"可强制退出! ')
+            # 加一层判断可以在运行过程中还能退出
+            if user_input == 'exit':
+                return
+            if user_input == i.e_word:
+                print('正确')
+                count += 1
+            else:
+                print(f'错误, 正确的拼写应该为{i.e_word}')
+                error_word[i.e_translation] = i.e_word
+        else:
+            score = count / len(result) * 100
+            print(f'总共答对的次数为{count}, 正确率{count / len(result):.2%}, 得分为{score:.1f}')
+        if count / len(result) < 1:
+            while True:
+                if len(error_word) > 0:
+                    continue_exercise = input('继续错误习题练习请输入1,输入其他返回主菜单 ')
+                    if continue_exercise == '1':
+                        count_two = 0
+                        ec = error_word.copy()
+                        for i in ec:
+                            if input(f'根据意思拼写单词, {i} ') == error_word[i]:
+                                print('正确')
+                                count_two += 1
+                                del error_word[i]
+                            else:
+                                print(f'错误, 正确的拼写应该为{error_word[i]}')
+                        if len(error_word) == 0:
+                            print('再次练习,正确率为100%')
+                            break
+                        print(f'二次练习答对的次数为{count_two}, 正确率{count_two / len(ec):.2%}')
+                    else:
+                        break
+
 
 
 if __name__ == '__main__':
@@ -162,5 +214,9 @@ if __name__ == '__main__':
             ls.learning_spell(session)
         elif result == '6':
             ls.translation(session)
+
+        elif result == '7':
+            ls.get_random_100(session)
+
         else:
             break
